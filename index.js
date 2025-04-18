9bf1c321603d87771e8274853c3af3e201056fa9
Natejpg <nathan11.abreu@gmail.com>
2025-04-17 23:59:14 -0300

    feat: Adicionando projeto 3 do curso de backend da FreeCodeCamp

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.json());
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -22,3 +25,45 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+const urls = {};
+
+let idCounter = 1;
+
+app.post('/api/shorturl', (req, res) => {
+
+  const originalUrl = req.body.url;
+
+  try{
+
+    const urlObj = new URL(originalUrl);
+
+  }catch{
+
+    return res.json({error: 'invalid url'})
+  }
+
+  const id = idCounter++;
+  urls[id] = originalUrl;
+
+  res.json({
+    original_url: originalUrl,
+    short_url: id
+  });
+});
+
+app.get('/api/shorturl/:id', (req, res) => {
+
+  const id = req.params.id;
+  const url = urls[id];
+
+  if(url){
+    res.redirect(url);
+  }else{
+    res.json({error: "No short URL found for the given input"})
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000");
+})
